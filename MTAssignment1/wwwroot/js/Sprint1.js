877edbccba3124ed69dad55c3c3e48aa9f86e98e
Majid Tooranisama <mtooranisama5070@conestogac.on.ca>
2019-01-31 23:00:26 -0500

    Initial commit

---
+++ MTAssignment1/wwwroot/js/Sprint1.js
@@ -0,0 +1,6 @@
+﻿function BackColor(color) {
+    var myText = document.getElementById("US1originalText").value;    
+    document.getElementById("greenBackground").value = myText;
+    document.getElementById("greenBackground").style.backgroundColor = color;
+};
+
